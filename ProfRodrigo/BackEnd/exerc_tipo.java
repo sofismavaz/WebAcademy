e241aa55d9706c62fcc16ead63268ce5b7db044e
Lucir Vaz <sofisma.vaz@gmail.com>
2025-09-17 18:47:00 -0500

    feat:Tipo Tiposde conteúdos

---
+++ ProfRodrigo/BackEnd/exerc_tipo.java
@@ -0,0 +1,17 @@
+package BackEnd;
+
+public class exerc_tipo {
+    public static void main(String[] args) {
+        int numero = 10;
+        double decimal = 5.5;
+        String texto = "Olá, Mundo!";
+        boolean verdadeiroFalso = true;
+        float numeroFloat = 3.14f;
+
+        System.out.println("Número inteiro: " + numero);
+        System.out.println("Número decimal: " + decimal);
+        System.out.println("Texto: " + texto);
+        System.out.println("Valor booleano: " + verdadeiroFalso);
+        System.out.println("Número float: " + numeroFloat);
+    }
+}
